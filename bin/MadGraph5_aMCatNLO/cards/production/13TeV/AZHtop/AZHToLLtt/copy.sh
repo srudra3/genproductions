37a3317e42d04485db24c867a43c60cd14d90540
sahithi <srudra4@uic.edu>
2021-07-27 13:07:16 +0200

    updates

--- bin/MadGraph5_aMCatNLO/cards/production/13TeV/AZHtop/AZHToLLtt/copy.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/13TeV/AZHtop/AZHToLLtt/copy.sh
@@ -2,20 +2,24 @@
 
 default=$(basename $(pwd))
 #m=$1
-for m in 400 500 600 700 750 800 900 1000 1200 1400 1600 
+for mA in 1000  
 do
-    echo Copying mass M$m
-    newdir=../"$default"_M"$m"
+for mH in 600
+do
+    echo "Copying mass (mA,mH) ($mA,$mH)"
+    newdir="$default"_mA"$mA"_mH"$mH"
     mkdir $newdir
-    cp "$default"_customizecards.dat $newdir/"$default"_M"$m"_customizecards.dat
-    cp "$default"_extramodels.dat $newdir/"$default"_M"$m"_extramodels.dat
-#    cp "$default"_madspin_card.dat $newdir/"$default"_M"$m"_madspin_card.dat
-    cp "$default"_proc_card.dat $newdir/"$default"_M"$m"_proc_card.dat
-    cp "$default"_run_card.dat $newdir/"$default"_M"$m"_run_card.dat
+    cp "$default"_customizecards.dat $newdir/"$default"_mA"$mA"_mH"$mH"_customizecards.dat
+    cp "$default"_extramodels.dat $newdir/"$default"_mA"$mA"_mH"$mH"_extramodels.dat
+    cp "$default"_madspin_card.dat $newdir/"$default"_mA"$mA"_mH"$mH"_madspin_card.dat
+    cp "$default"_proc_card.dat $newdir/"$default"_mA"$mA"_mH"$mH"_proc_card.dat
+    cp "$default"_run_card.dat $newdir/"$default"_mA"$mA"_mH"$mH"_run_card.dat
     # modify output name
-    sed -i 's/'$default'/'$default'_M'$m'/g' $newdir/"$default"_M"$m"_proc_card.dat
+    sed -i 's/'$default'/'$default'_mA'$mA'_mH'$mH'/g' $newdir/"$default"_mA"$mA"_mH"$mH"_proc_card.dat
     # Modify mass parameter
-    sed -i 's/AMASS/'$m'.0/g' $newdir/"$default"_M"$m"_customizecards.dat
+    sed -i 's/AMASS/'$mA'.0/g' $newdir/"$default"_mA"$mA"_mH"$mH"_customizecards.dat
+    sed -i 's/HMASS/'$mH'.0/g' $newdir/"$default"_mA"$mA"_mH"$mH"_customizecards.dat
+done
 done
 
 #./../../../../../gridpack_generation.sh AZHToLLtt AZHToLLtt/ #1nd
